1850ddab65fb68dcea873612bfaa38f9fe2f02ef
Jean Lucas Monte Carvalho <jmontecarvalho@gmail.com>
2018-12-12 20:27:43 -0200

    Added connect and disconnect functions

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const path = require('path');
 const express = require('express')
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+const socketIO = require('socket.io')
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, ()=> {
+io.on('connection', (socket) => {
+    console.log('new user connected')
+    socket.on('disconnect', ()=> {
+        console.log('User disconnected')
+    });
+});
+
+server.listen(port, ()=> {
     console.log(`Server started on port ${port}.`)
 });
